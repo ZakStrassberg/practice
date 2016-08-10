e804e82165fe520ede4ff7ae4b47ef6ac27e4f15
Zak Strassberg <zakstrassberg@gmail.com>
2016-08-10 14:27:38 -0700

    implmented filter

--- underscoreLibrary.py
+++ underscoreLibrary.py
@@ -10,11 +10,23 @@ class Underscore(object):
             returnValue += iteratee(0, each)
         return returnValue
 
-    def find(self):
-        pass
+    def find(self, list, predicate):
+        for each in list:
+            if predicate(each):
+                return each
+        return None
 
-    def filter(self):
-        pass
+    def filter(self, list, predicate):
+        filterList = []
+        foundItem = False
+        for each in list:
+            if predicate(each):
+                filterList.append(each)
+                foundItem = True
+        if foundItem:
+            return filterList
+        else:
+            return None
 
     def reject(self):
         pass
@@ -22,10 +34,12 @@ class Underscore(object):
 
 # you just created a library with 5 methods!
 # let's create an instance of our class
-_ = Underscore()  # yes we are setting our instance to a variable that is an underscore
+_ = Underscore()
 
 print _.map([1, 2, 3], lambda x: x ** 2)
 print _.reduce([1, 2, 3], lambda memo, num: memo + num, 0)
+print _.find([1, 3, 3, 4, 5, 6], lambda num: num % 2 == 0)
+print _.filter([1, 2, 3, 4, 5, 6], lambda num: num % 2 == 0)
 
 # evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # should return [2, 4, 6] after you finish implementing the code above
