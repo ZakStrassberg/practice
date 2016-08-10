44a4a1aaa3352fd01815247920f58746fa20a253
Zak Strassberg <zakstrassberg@gmail.com>
2016-08-10 13:58:49 -0700

    very bad reduce function

--- underscoreLibrary.py
+++ underscoreLibrary.py
@@ -2,8 +2,13 @@ class Underscore(object):
     def map(self, list, iteratee):
         return [iteratee(each) for each in list]
 
-    def reduce(self, list, iteratee, *memo):
-        pass
+    def reduce(self, list, iteratee, memo=None):
+        if memo is None:
+            memo = list[0]
+        returnValue = memo
+        for each in list:
+            returnValue += iteratee(0, each)
+        return returnValue
 
     def find(self):
         pass
@@ -20,7 +25,8 @@ class Underscore(object):
 _ = Underscore()  # yes we are setting our instance to a variable that is an underscore
 
 print _.map([1, 2, 3], lambda x: x ** 2)
+print _.reduce([1, 2, 3], lambda memo, num: memo + num, 0)
 
-evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+# evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # should return [2, 4, 6] after you finish implementing the code above
-print evens
+# print evens
