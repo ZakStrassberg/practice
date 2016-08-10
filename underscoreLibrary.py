6f7dcfd22de9368946af515c23eb54d127b51e2d
Zak Strassberg <zakstrassberg@gmail.com>
2016-08-10 13:00:44 -0700

    formatting and pep8

--- underscoreLibrary.py
+++ underscoreLibrary.py
@@ -3,26 +3,27 @@ class Underscore(object):
         mappedList = []
         for each in list:
             mappedList.append(iteratee(each))
-
         return mappedList
+
     def reduce(self):
         pass
+
     def find(self):
         pass
+
     def filter(self):
         pass
+
     def reject(self):
         pass
 
 
 # you just created a library with 5 methods!
 # let's create an instance of our class
-_ = Underscore() # yes we are setting our instance to a variable that is an underscore
+_ = Underscore()  # yes we are setting our instance to a variable that is an underscore
 
-print _.map([1,2,3], lambda x: x ** 2)
+print _.map([1, 2, 3], lambda x: x ** 2)
 
 evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # should return [2, 4, 6] after you finish implementing the code above
 print evens
-
-
